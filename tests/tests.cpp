068028b2c02465b9d4320ef7e279f53b19b6140b
Julian Konowalski <168512028+JulianKonowalski@users.noreply.github.com>
2025-02-07 23:17:47 +0100

    Delete tests directory

--- tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#ifndef GTEST
-	#define GTEST
-	#include <gtest/gtest.h> //multiple includes of this header cause compilation errors
-#endif // !GTEST
-
-int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
